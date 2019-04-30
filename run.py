be2192c767db4e1ed9393fdabb767556785adf1d
Jordan396 <jordan.chyehong@gmail.com>
2019-04-30 14:52:25 -0400

    Fixed error in getting OS variable

--- run.py
+++ run.py
@@ -30,9 +30,9 @@ Todo:
    http://google.github.io/styleguide/pyguide.html
 
  @author: Jordan396 <https://github.com/Jordan396/Trivial-Twitter-Sockets>
-                                                                                                                               
-You should have received a copy of the MIT License when cloning this   
-repository. If not, see <https://opensource.org/licenses/MIT>.         
+
+You should have received a copy of the MIT License when cloning this
+repository. If not, see <https://opensource.org/licenses/MIT>.
 """
 
 import os
@@ -63,10 +63,9 @@ app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 with open('/tmp/gsa.json', 'w') as outfile:
-    json.dump(ast.literal_eval(os.environ.get(
-        'GOOGLE_SERVICE_ACCOUNT')), outfile)
+    json.dump(ast.literal_eval(os.environ['GOOGLE_SERVICE_ACCOUNT']), outfile)
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/tmp/gsa.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/tmp/gsa.json"
 
 
 @app.route("/<path:path>")
@@ -74,26 +73,26 @@ def catch_all(path):
     return render_template("index.html")
 
 
-@app.route("/api/transcribe-audio", methods=["POST"])
+@app.route("/api/transcribe-audio", methods = ["POST"])
 def transcribeAudio():
-    user_email = request.form["user-email"]
+    user_email=request.form["user-email"]
     # check if the post request has the file part
     if "file" not in request.files:
         print("No file part")
         return render_template("index.html")
-    file = request.files["file"]
+    file=request.files["file"]
     # if user does not select file, browser also
     # submit an empty part without filename
     if file.filename == "":
         print("No selected file")
         return render_template("index.html")
     if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        source_file_name = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+        filename=secure_filename(file.filename)
+        source_file_name=os.path.join(app.config["UPLOAD_FOLDER"], filename)
         # Get current time for file identification
-        now = datetime.datetime.now()
-        timeIdentifier = now.strftime("%Y%m%d%H%M")
-        gcp_filename = "{}|audio{}.wav".format(user_email, timeIdentifier)
+        now=datetime.datetime.now()
+        timeIdentifier=now.strftime("%Y%m%d%H%M")
+        gcp_filename="{}|audio{}.wav".format(user_email, timeIdentifier)
         file.save(source_file_name)
         upload_blob(AUDIO_BUCKET_NAME, source_file_name, gcp_filename)
     else:
@@ -102,17 +101,17 @@ def transcribeAudio():
 
 
 def allowed_file(filename):
-    hasValidExtension = "." in filename and filename.rsplit(
+    hasValidExtension="." in filename and filename.rsplit(
         ".", 1)[1].lower() in ALLOWED_EXTENSIONS
-    hasValidFilename = "|" not in filename
+    hasValidFilename="|" not in filename
     return (hasValidExtension and hasValidFilename)
 
 
 def upload_blob(bucket_name, source_file_name, destination_blob_name):
     """Uploads a file to the bucket."""
-    storage_client = storage.Client()
-    bucket = storage_client.get_bucket(bucket_name)
-    blob = bucket.blob(destination_blob_name)
+    storage_client=storage.Client()
+    bucket=storage_client.get_bucket(bucket_name)
+    blob=bucket.blob(destination_blob_name)
 
     blob.upload_from_filename(source_file_name)
 
@@ -121,4 +120,4 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host = '0.0.0.0')
